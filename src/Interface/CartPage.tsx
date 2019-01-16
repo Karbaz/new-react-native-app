e3589da0f753adab3ea8790f08e16cb481a8cc23
karbaz <arbaz.k@bewakoof.com>
2019-01-16 15:41:43 +0530

    added cart interface

---
+++ src/Interface/CartPage.tsx
@@ -0,0 +1,16 @@
+interface cart_component {
+    props: {
+        left: {
+            styles?: any,
+            text: any
+        },
+        right: {
+            styles?: any,
+            text?: any
+        }
+    }
+}
+
+export interface cart_interface {
+    components?: any,
+}
